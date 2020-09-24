598be4e1b9869561708a2769d83a31ef52202107
Ryan Lee <rjlguitar@gmail.com>
2020-09-24 12:22:03 -0700

    added custom order function

--- poetryslam.py
+++ poetryslam.py
@@ -64,3 +64,30 @@ def lines_printed_random():
         random.shuffle(lines_list)
         print(i)
 
+#function to print in alphabetical order
+def lines_printed_custom():
+    lines_list = [
+        '"Smart" by Shel Silverstein', 
+        'My dad gave me one dollar bill', 
+        "Cause I'm his smartest son,", 
+        'And I swapped it for two shiny quarters', 
+        "'Cause two is more then one!", 
+        'And then I took the quarters', 
+        'And traded them to Lou', 
+        "For three dimes-- I guess he didn't know", 
+        'That three is more than two!', 
+        'Just then, along came old blind Bates', 
+        "And just 'cause he can't see", 
+        'He gave me four nickels for my three dimes,', 
+        'And four is more than three!', 
+        'And I took the nickels to Hiram Coombs', 
+        'Down at the seed-feed store,',   
+        'And the fool gave me five pennies for them,', 
+        'And five is more than four!', 
+        'And I went and showed my dad,', 
+        'And he got red in the cheeks', 
+        'And closed his eyes and shook his head--', 
+        'Too proud of me to speak!'
+    ]
+    for i in sorted(lines_list):
+        print(i)
